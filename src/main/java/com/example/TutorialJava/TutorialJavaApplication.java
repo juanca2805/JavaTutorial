049e96e65b7937e866425a6f1e201524df38b4da
juanca0108 <109818872+juanca2805@users.noreply.github.com>
2024-06-11 15:17:01 -0500

    Add loggers and routes

--- src/main/java/com/example/TutorialJava/TutorialJavaApplication.java
+++ src/main/java/com/example/TutorialJava/TutorialJavaApplication.java
@@ -1,13 +1,19 @@
 package com.example.TutorialJava;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TutorialJavaApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(TutorialJavaApplication.class);
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialJavaApplication.class, args);
+		logger.error("ta mal");
 	}
 
 }
