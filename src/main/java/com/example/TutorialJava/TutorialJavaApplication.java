6ef89fd2f28a274069b4ec6d5b804d4f345cb93d
juanca0108 <109818872+juanca2805@users.noreply.github.com>
2024-06-11 13:19:30 -0500

    Create new proyect

---
+++ src/main/java/com/example/TutorialJava/TutorialJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TutorialJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TutorialJava/TutorialJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TutorialJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
